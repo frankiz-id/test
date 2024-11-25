dd434f6f49de70ddf3d82375d3e762f9d7acdd2b
Nikita <nikita.kopin.hagen@gmail.com>
2024-11-25 22:27:18 +0500

    second commit

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -3,6 +3,7 @@ package org.example;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+
 public class Bot extends TelegramLongPollingBot {
 
     @Override
@@ -17,7 +18,13 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update);
+        if(update.hasMessage() && update.getMessage().hasText()){
+            if(update.getMessage().getText().equals("/start")){
+                System.out.println("Сработал /start");
+            }
+        }else if(update.hasCallbackQuery()){
+            System.out.println("Сработал hasCallbackQuery");
+        }
     }
 
 }
